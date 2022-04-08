d3162ed45ba00afb3ed9079626ba20f69e2b7f18
Priyanka <paulpriyanka2134@gmail.com>
2022-04-08 17:44:15 +0530

    finally

--- src/test/java/org/gitcmd/cmd.java
+++ src/test/java/org/gitcmd/cmd.java
@@ -2,19 +2,24 @@ package org.gitcmd;
 
 public class cmd {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
 		
 		int a=1;
 		int b=2;
 		
-		int c=a*b;
-		
-		System.out.println(c);
 		
 		
-				
-
+		System.out.println(multiplication(a,b));
 	}
+		
+		public static int multiplication(int a,int b)
+				
+		{
+			return(a*b);
+		
+		}
+		
 
 }
